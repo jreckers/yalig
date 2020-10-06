2af964312d4b9049bbb08199c17c75f2173ea83c
jreckers <joe.reckers@gmail.com>
2020-10-06 08:21:14 -0400

    2020-1006 - 0821

--- source/background.js
+++ source/background.js
@@ -32,12 +32,14 @@ function createTodayTime() {
   var goTime = new Date();
   var currentHours = ("0" + goTime.getHours()).substr(-2);
   var currentMins = ("0" + goTime.getMinutes()).substr(-2);
-  var currentMonth = ("0" + goTime.getMonth()).substr(2);
-  var currentDay = ("0" + goTime.getDay()).substr(-2);
+  var currentMonth = ("0" + (goTime.getMonth()+1)).substr(-2);
+  var currentDate = ("0" + goTime.getDate()).substr(-2);
+  var currentDay = ("0" + (goTime.getDay())).substr(-2);
 
   console.log(
     "G0-" + currentMonth + currentDay + " - " + currentHours + currentMins
   );
+//  console.log(goTime);
   console.log(goTime.toLocaleDateString());
 
   return formatGoTime(today);
---
+++ source/yalig-goTime.js
@@ -0,0 +1,14 @@
+//jore001
+
+  var goTime = new Date();
+  var currentHours = ("0" + goTime.getHours()).substr(-2);
+  var currentMins = ("0" + goTime.getMinutes()).substr(-2);
+  var currentMonth = ("0" + (goTime.getMonth()+1)).substr(-2);
+  var currentDate = ("0" + goTime.getDate()).substr(-2);
+  var currentDay = ("0" + (goTime.getDay())).substr(-2);
+
+  console.log(currentMonth);
+  console.log(currentDay);
+  console.log(currentHours);
+  console.log(currentMins);
+  console.log(currentHours + ":" + currentMins)
